57cc8fa8343a157d2974940c409058dbaef9490d
Suriya Kandaswamy <sardines007@gmail.com>
2017-02-19 01:41:29 -0500

    oops

--- question.py
+++ question.py
@@ -19,9 +19,9 @@ if (__name__ == '__main__'):
     numf.write(str(fnum+1))
     numf.close()
 
-    op_filename = 'question'+str(fnum)+'.ogg'
+    op_filename = 'question'+str(fnum)+'.wav'
 
-    command = ['curl', '-X', 'POST', '-u', '773f9f5b-6452-4d6e-9aa3-17a0c2674eed:5uDZ7hPl43y4', '--header', 'Content-Type: application/json', '--header', 'Accept: audio/ogg', 'https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize', '--data', '{\"text\":\"' + question + '\"}', '--output', op_filename, 'https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize']
+    command = ['curl', '-X', 'POST', '-u', '773f9f5b-6452-4d6e-9aa3-17a0c2674eed:5uDZ7hPl43y4', '--header', 'Content-Type: application/json', '--header', 'Accept: audio/wav', 'https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize', '--data', '{\"text\":\"' + question + '\"}', '--output', op_filename, 'https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize']
     subprocess.call(command,stdout=FNULL,stderr=subprocess.STDOUT)
 
     print(op_filename)
