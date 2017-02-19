67e149123ffbe94daaa9b17d671c5e79ccd89447
chzozu <chzou2009@gmail.com>
2017-02-19 03:09:49 -0800

    minor updates

--- question.py
+++ question.py
@@ -7,7 +7,7 @@ import random
 
 if (__name__ == '__main__'):
     word = sys.argv[1]
-    adds = ["What are you doing with that " + word + "?", "Why is there that " + word + "?", "How do you feel about that " + word + "?"
+    adds = ["What are you doing with that " + word + "?", "Why is there that " + word + "?", "How do you feel about that " + word + "?"]
     question = "That is a " + word + ". " + adds[random.randint(0,2)]
 
     numf = open('questionnum.txt','r')
